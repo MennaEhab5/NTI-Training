e4f552c834a5954ade9c0a3ad1ff613c117decb5
Menna Ehab <menna28112005@gmail.com>
2025-08-10 14:32:22 +0300

    S11

---
+++ S11/js/custom.js
@@ -0,0 +1,44 @@
+// console.log("hello");
+// var let const
+// x=5
+// var z=3
+// var z="hello"
+
+// let a=4
+// let a="hello"
+
+// if("true"){
+//     var z="test"
+//     let a=3
+//     console.log(a, z)
+// }
+//     console.log(a, z)
+
+// let name= prompt("Hello please Enter your name")
+// name = Number(name)
+// name = String(name)
+// name = Boolean(name)
+// console.log(name, typeof name)
+
+// let num1= Number(prompt("Enter the first number"))
+// let num2= Number(prompt("Enter the second number"))
+
+// let result= num1+num2
+// console.log("Sum "+result)
+// alert("Sum "+(num1+num2))
+
+// let num= Number(prompt("Enter number in Celsius"))
+// let result= num*(9/5) +32
+// alert("number in Fahrenheit " +result)
+
+// let x=3
+// console.log(x++)
+// console.log(x)
+// console.log(++x)
+// console.log(x)
+
+// let x="5"
+// console.log(x === 5)
+
+// let x=5
+// x == 5? console.log("x = 5"): console.log("x msh b 5")
