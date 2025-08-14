30f7d17b0cfd386e25ba4603e902448806705341
Menna Ehab <menna28112005@gmail.com>
2025-08-14 14:02:01 +0300

    project

--- trying/js/main.js
+++ trying/js/main.js
@@ -8,24 +8,24 @@ window.addEventListener("scroll", () => {
 })
 
 
-const smallThumbs = document.querySelectorAll(".small-thumb");
-const bigImage = document.getElementById("bigImage");
+// const smallThumbs = document.querySelectorAll(".small-thumb");
+// const bigImage = document.getElementById("bigImage");
 
-smallThumbs[0].classList.add("active");
+// smallThumbs[0].classList.add("active");
 
-smallThumbs.forEach((img) => {
-  img.addEventListener("click", function () {
-    if (this.classList.contains("active")) return;
-    smallThumbs.forEach((t) => t.classList.remove("active"));
-    this.classList.add("active");
+// smallThumbs.forEach((img) => {
+//   img.addEventListener("click", function () {
+//     if (this.classList.contains("active")) return;
+//     smallThumbs.forEach((t) => t.classList.remove("active"));
+//     this.classList.add("active");
 
-    bigImage.classList.add("fade-in");
-    setTimeout(() => {
-      bigImage.src = this.src;
-      bigImage.classList.remove("fade-in");
-    }, 400);
-  });
-});
+//     bigImage.classList.add("fade-in");
+//     setTimeout(() => {
+//       bigImage.src = this.src;
+//       bigImage.classList.remove("fade-in");
+//     }, 400);
+//   });
+// });
 
 //   const search = document.getElementById("#search");
 //   const innerSearch = document.getElementById("#inner-search");
